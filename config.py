91da66e22dfa40de11ee9fe7c4388283b381fd5e
ishank dubey <ishankdubey@gmail.com>
2023-10-02 19:12:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,38 @@ import shutil
 from IPython.display import HTML, display
 from tensorboard import manager
 
-def tensorboard_cleanup():
-    info_dir = manager._get_info_dir()
-    shutil.rmtree(info_dir)
-FOLDERS = {
-    0: ['plots'],
-    1: ['plots'],
-    2: ['plots', 'data_generation', 'data_preparation', 'model_configuration', 'model_training'],
-    21: ['plots', 'data_generation', 'data_preparation', 'model_configuration', 'stepbystep'],
-    3: ['plots', 'stepbystep'],
-    4: ['plots', 'stepbystep', 'data_generation'],
-    5: ['plots', 'stepbystep', 'data_generation', ''],
-    6: ['plots', 'stepbystep', 'stepbystep', 'data_generation', 'data_generation', 'data_preparation'],
-    7: ['plots', 'stepbystep', 'data_generation'],
-    71: ['plots', 'stepbystep', 'data_generation'],
-    8: ['plots', 'plots', 'stepbystep', 'data_generation'],
-    9: ['plots', 'plots', 'plots', 'stepbystep', 'data_generation'],
-    10: ['plots', 'plots', 'plots', 'plots', 'stepbystep', 'data_generation', 'data_generation', '', ''],
-    11: ['plots', 'stepbystep', 'data_generation', ''],
-}
+
+def download_to_colab(branch='master'):
+    base_url = 'https://raw.githubusercontent.com/Ishank-dubey/unSupervisedLearning/master/{}/'.format(branch)
+
+    folders = ['data_generation', 'images', 'plots', 'runs']
+    for folder in folders:
+        print('IN')
+        if len(folder):
+            try:
+                os.mkdir(folder)
+            except OSError as e:
+                print('RAISe')
+                if e.errno != errno.EEXIST:
+                    raise
+    path = os.path.join('data_generation', 'image_classification.py')
+    url = '{}{}'.format(base_url, path)
+    r = requests.get(url, allow_redirects=True)
+    open(path, 'wb').write(r.content)
+
+    path = os.path.join('StepByStep.py')
+    url = '{}{}'.format(base_url, path)
+    r = requests.get(url, allow_redirects=True)
+    open(path, 'wb').write(r.content)
+
+    path = os.path.join('helpers.py')
+    url = '{}{}'.format(base_url, path)
+    r = requests.get(url, allow_redirects=True)
+    open(path, 'wb').write(r.content)
+
+    path = os.path.join('plots', 'chapter5.py')
+    url = '{}{}'.format(base_url, path)
+    r = requests.get(url, allow_redirects=True)
+    open(path, 'wb').write(r.content)
+
+##download_to_colab()
